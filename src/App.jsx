c3c371cba6b8276dd12c7b6c07da9823f9c88de6
sangam verma <sangamverma955911@gmail.com>
2025-05-03 16:50:32 +0530

    resolve error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './components/home/Home';
-import ShowList from './components/ShowList/ShowList';
+import ShowList from './components/showlist/ShowList';
 import GetPaid from './components/getPaid/GetPaid';
 import Analytics from './components/analytics/Analytics';
 import Showanalytics from './components/analytics/Showanalytics';
